2a5b4c86034ca13c369f11648f35c813aebc59d5
MaksBoltruczuk <maksymilian.boltruczuk@infotech.edu.pl>
2024-12-06 11:43:13 +0100

    Dodano zadanie 61.1

--- cke/zad61.py
+++ cke/zad61.py
@@ -1,10 +1,25 @@
 ciagi = []
 with open("ciagi.txt") as plik:
     for linia in plik:
-        liczby=[]
+        liczby = []
         for liczba in linia.split():
             liczby.append(int(liczba))
         if len(liczby) > 1:
             ciagi.append(liczby)
 
 print(ciagi)
+
+licznik = 0
+maks = 0
+for ciag in ciagi:
+    roznica = ciag[1] - ciag[0]
+    for i in range(1, len(ciag)):
+        if roznica != ciag[i]-ciag[i-1]:
+            break
+    else:
+        licznik += 1
+        if roznica > maks:
+            maks = roznica
+
+
+print(licznik, maks)
